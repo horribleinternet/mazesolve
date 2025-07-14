f995dd4b764d095158e792e26cfb816a5782c54f
horribleinternet <cyberblah@gmail.com>
2025-07-15 00:55:08 +0900

    test line draw

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from window import Window
+from line import Point, Line
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(100, 100), Point(700, 500)), "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
