df84b92ed4674b1ad261c23148a2dba662887782
horribleinternet <cyberblah@gmail.com>
2025-07-15 00:54:22 +0900

    classes to draw a line

---
+++ line.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, color):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=color, width=2)
