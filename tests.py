f3756326cefbf7b393a6757dfad323fd61e58882
horribleinternet <cyberblah@gmail.com>
2025-07-17 01:54:37 +0900

    test reseting visted cells

--- tests.py
+++ tests.py
@@ -29,8 +29,8 @@ class Tests(unittest.TestCase):
         )
 
     def test_maze_create_cells_large(self):
-        num_cols = 323
-        num_rows = 117
+        num_cols = 32
+        num_rows = 24
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._Maze__cells),
@@ -48,8 +48,16 @@ class Tests(unittest.TestCase):
         self.assertFalse(m1._Maze__cells[0][0].has_top_wall)
         self.assertFalse(m1._Maze__cells[-1][-1].has_bottom_wall)
         self.assertFalse(m1._Maze__cells[19][14].has_bottom_wall)
-        self.assertTrue(m1._Maze__cells[5][5].has_top_wall)
-        self.assertTrue(m1._Maze__cells[10][10].has_bottom_wall)
+
+    def test_maze_visited_reset(self):
+        num_cols = 20
+        num_rows = 15
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        all = False
+        for col in m1._Maze__cells:
+            for cell in col:
+                all = all or cell.visited
+        self.assertFalse(all)
 
 if __name__ == "__main__":
     unittest.main()
