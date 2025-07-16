d07f677ce84de9cddc0b7455237c8574c3f9cbd2
horribleinternet <cyberblah@gmail.com>
2025-07-16 21:30:33 +0900

    add cell drawing primitive

---
+++ cell.py
@@ -0,0 +1,27 @@
+from line import Point, Line
+
+class Cell:
+    def __init__(self, window):
+        self.__win = window
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__tl = None
+        self.__bl = None
+        self.__tr = None
+        self.__br = None
+
+    def draw(self, point_tl, point_br, color = "black"):
+        self.__tl = Point(point_tl.x, point_tl.y)
+        self.__bl = Point(point_tl.x, point_br.y)
+        self.__tr = Point(point_br.x, point_tl.y)
+        self.__br = Point(point_br.x, point_br.y)
+        if self.has_left_wall:
+            self.__win.draw_line(Line(self.__bl, self.__tl))
+        if self.has_top_wall:
+            self.__win.draw_line(Line(self.__tl, self.__tr))
+        if self.has_right_wall:
+            self.__win.draw_line(Line(self.__tr, self.__br))
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(self.__br, self.__bl))
