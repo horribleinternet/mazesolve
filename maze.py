3f223ba2e53a14bbec0c68dff29373b0984fa9a3
horribleinternet <cyberblah@gmail.com>
2025-07-17 00:25:12 +0900

    open mazed entrance and exit

--- maze.py
+++ maze.py
@@ -20,8 +20,13 @@ class Maze:
         for i in range(len(self.__cells)):
             for j in range(len(self.__cells[0])):
                 self.__draw_cell(i, j)
+        self.__break_entrance_and_exit()
 
     def __draw_cell(self, i, j):
+        if (i < 0):
+            i = self.num_cols + i
+        if (j < 0):
+            j = self.num_rows + j
         x1 = self.x1 + i * self.cell_size_x
         y1 = self.y1 + j * self.cell_size_y
         self.__cells[i][j].draw(x1, y1, x1 + self.cell_size_x, y1 + self.cell_size_y, self.color)
@@ -32,3 +37,9 @@ class Maze:
             return
         self.win.redraw()
         time.sleep(self.anim_sleep)
+
+    def __break_entrance_and_exit(self):
+        self.__cells[0][0].has_top_wall = False
+        self.__draw_cell(0, 0)
+        self.__cells[-1][-1].has_bottom_wall = False
+        self.__draw_cell(-1, -1)
